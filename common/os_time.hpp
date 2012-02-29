851b54658c76f466b47e25cd95a8fd704efff4be
Jeff Muizelaar <jmuizelaar@mozilla.com>
2012-02-29 09:58:32 -0500

    Implement getTime for OS X
    mach_absolute_time lowest level timing function on OS X
    
    Signed-off-by: Jose Fonseca <jose.r.fonseca@gmail.com>

--- common/os_time.hpp
+++ common/os_time.hpp
@@ -35,6 +35,8 @@
 #include <windows.h>
 #elif defined(__linux__)
 #include <time.h>
+#elif defined(__APPLE__)
+#include <mach/mach_time.h>
 #else
 #include <sys/time.h>
 #endif
@@ -43,14 +45,14 @@
 namespace os {
 
     // OS dependent time frequency
-#if defined(_WIN32)
-    // runtime variable on Windows
+#if defined(_WIN32) || defined(__APPLE__)
+    // runtime variable on Windows and MacOSX
     extern long long timeFrequency;
 #elif defined(__linux__)
-    // nanoseconds
+    // nanoseconds on Linux
     static const long long timeFrequency = 1000000000;
 #else
-    // microseconds on
+    // microseconds on Unices
     static const long long timeFrequency = 1000000;
 #endif
 
@@ -72,6 +74,13 @@ namespace os {
             return 0;
         }
         return tp.tv_sec * 1000000000LL + tp.tv_nsec;
+#elif defined(__APPLE__)
+        if (!timeFrequency) {
+            mach_timebase_info_data_t timebaseInfo;
+            mach_timebase_info(&timebaseInfo);
+            timeFrequency = 1000000000 * timebaseInfo.denom / timebaseInfo.numer;
+        }
+        return mach_absolute_time();
 #else
         struct timeval tv;
         gettimeofday(&tv, NULL);
